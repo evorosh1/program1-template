b614505caaf577f3864a146f99c6636a5b1921b7
evorosh1 <32557575+evorosh1@users.noreply.github.com>
2019-02-27 18:13:49 -0500

    Add files via upload

---
+++ Star.h
@@ -0,0 +1,36 @@
+Star.h 
+#ifndef Star_Planet
+	#define Star_Planet
+	#include "Planet.h"
+	#include "List.h"
+	#include "Vector.h"
+	
+	class Starvector{
+	private:
+		unsigned int current_p;
+	public:
+		Vector *vect;
+		Starvector();
+		~Starvector();
+	  long addPlanet();
+		bool removePlanet(long);
+		Planet * getPlanet(long);
+		void orbit();
+		void printStarInfo();
+		unsigned int getCurrentNumPlanets(){return this->current_p;}
+	};
+	
+	class Starlist{
+	public:
+		List *list;
+		Starlist();
+		~Starlist();
+		long addPlanet();
+		bool removePlanet(long);
+		Planet * getPlanet(long);
+		void orbit();
+		void printStarInfo();
+		unsigned int getCurrentNumPlanets();
+	};
+	
+	#endif
